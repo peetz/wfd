2c7b42e804aa6a26e9b15a0a1141e618fb78664e
peetz <stephen.peters@me.com>
2026-08-22 20:24:51 +0100

    Add meal library sensor entity

---
+++ custom_components/wfd/sensor.py
@@ -0,0 +1,33 @@
+"""WFD sensor entities."""
+
+from __future__ import annotations
+
+from homeassistant.components.sensor import SensorEntity
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up WFD sensors."""
+    data = hass.data["wfd"][entry.entry_id]
+    async_add_entities([WFDMealLibrarySensor(data["meal_library"])])
+
+
+class WFDMealLibrarySensor(SensorEntity):
+    """Expose the active WFD meal library."""
+
+    _attr_name = "WFD Meal Library"
+
+    def __init__(self, meal_library):
+        self._meal_library = meal_library
+        self._attr_unique_id = "wfd_meal_library"
+        self._meals = []
+
+    @property
+    def native_value(self):
+        return len(self._meals)
+
+    @property
+    def extra_state_attributes(self):
+        return {"meals": [meal.name for meal in self._meals]}
+
+    async def async_update(self):
+        self._meals = await self._meal_library.async_get_meals()
