195cb221f454d8abdbf66c84cfbf9736b971051e
peetz <stephen.peters@me.com>
2026-08-24 17:55:04 +0100

    04b.2: refresh WFD sensors after mutations

--- custom_components/wfd/sensor.py
+++ custom_components/wfd/sensor.py
@@ -4,17 +4,20 @@ from __future__ import annotations
 
 try:
     from homeassistant.components.sensor import SensorEntity
+    from homeassistant.helpers.dispatcher import async_dispatcher_connect
 except ModuleNotFoundError:
     class SensorEntity:
         """Test fallback when Home Assistant is unavailable."""
+
+from .updates import SIGNAL_WFD_UPDATED
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
     """Set up WFD sensors."""
     data = hass.data["wfd"][entry.entry_id]
     async_add_entities([
-        WFDMealLibrarySensor(data["meal_library"]),
-        WFDHouseholdSensor(data["household"]),
+        WFDMealLibrarySensor(hass, data["meal_library"]),
+        WFDHouseholdSensor(hass, data["household"]),
     ])
 
 
@@ -23,10 +26,17 @@ class WFDMealLibrarySensor(SensorEntity):
 
     _attr_name = "WFD Meal Library"
 
-    def __init__(self, meal_library):
+    def __init__(self, hass, meal_library):
+        self._hass = hass
         self._meal_library = meal_library
         self._attr_unique_id = "wfd_meal_library"
         self._meals = []
+
+    async def async_added_to_hass(self):
+        async_dispatcher_connect(self._hass, SIGNAL_WFD_UPDATED, self._refresh)
+
+    def _refresh(self):
+        self.async_schedule_update_ha_state(True)
 
     @property
     def native_value(self):
@@ -34,16 +44,7 @@ class WFDMealLibrarySensor(SensorEntity):
 
     @property
     def extra_state_attributes(self):
-        return {
-            "meals": [
-                {
-                    "id": meal.id,
-                    "name": meal.name,
-                    "active": meal.active,
-                }
-                for meal in self._meals
-            ]
-        }
+        return {"meals": [{"id": m.id, "name": m.name, "active": m.active} for m in self._meals]}
 
     async def async_update(self):
         self._meals = await self._meal_library.async_get_meals(active_only=False)
@@ -54,11 +55,18 @@ class WFDHouseholdSensor(SensorEntity):
 
     _attr_name = "WFD Household"
 
-    def __init__(self, household):
+    def __init__(self, hass, household):
+        self._hass = hass
         self._household = household
         self._attr_unique_id = "wfd_household"
         self._voters = []
         self._available_persons = []
+
+    async def async_added_to_hass(self):
+        async_dispatcher_connect(self._hass, SIGNAL_WFD_UPDATED, self._refresh)
+
+    def _refresh(self):
+        self.async_schedule_update_ha_state(True)
 
     @property
     def native_value(self):
@@ -66,17 +74,7 @@ class WFDHouseholdSensor(SensorEntity):
 
     @property
     def extra_state_attributes(self):
-        return {
-            "voters": [
-                {
-                    "id": voter.id,
-                    "name": voter.name,
-                    "active": voter.active,
-                }
-                for voter in self._voters
-            ],
-            "available_persons": self._available_persons,
-        }
+        return {"voters": [{"id": v.id, "name": v.name, "active": v.active} for v in self._voters], "available_persons": self._available_persons}
 
     async def async_update(self):
         self._voters = await self._household.async_get_voters(active_only=False)
